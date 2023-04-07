0f7e15986f7947249c7798e98f434670591a1636
Bginna <billyginna@gmail.com>
2023-04-07 17:11:54 -0600

    bid amount is passed through to method

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,29 @@
     <div class="auction-app">
       <h1>Ethereum Auction Dapp</h1>
       <p>Current highest bid: {{ highestBid }}</p>
-      <input v-model="bidAmount" placeholder="Enter bid amount" />
-      <button @click="placeBid">Place Bid</button>
+      <form @submit.prevent="handleSubmit(bidAmount)">
+            <b-input-group>
+              <input
+                v-model="bidAmount" placeholder= "Enter Bid Amount"
+              />
+                <button>Place Bid</button>
+                <img
+                  v-show="isBid"
+                  id="isBid"                  >
+            </b-input-group>
+          </form>
     </div>
   </template>
+
+<script>
+
+
+
+export default {
+  methods: {
+    handleSubmit(bidAmount) {
+      console.log(bidAmount);
+    }
+  }
+}
+</script>
